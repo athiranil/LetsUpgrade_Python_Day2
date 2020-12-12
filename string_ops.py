83e76653af6e3e4b3bb5deefb7d2566e7af5f61a
athiranil <75883565+athiranil@users.noreply.github.com>
2020-12-13 00:01:41 +0530

    Delete string_ops.py

--- string_ops.py
+++
@@ -1,8 +0,0 @@
-a = input("Enter a string")
-print("Swapcase::",a.swapcase())
-print("Isalpha::",a.isalpha())
-print("upper case::",a.upper())
-n=len(a)-1
-print("Find '"+a[n]+"' ::",a.find(a[n]))
-print("Count '"+a[n]+"' ::",a.count(a[n]))
-print("replace '"+a[n]+"' with 'b'::",a.replace(a[n], "'b'"))
