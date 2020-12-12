364243a180c014a78f63eee4e18c6cd9140e8a1e
athiranil <75883565+athiranil@users.noreply.github.com>
2020-12-12 19:42:45 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,10 @@
+dic={"key1":"value1","key2":"value2","key3":"value3"}
+print("Dictionary::",dic)
+dic.update({"key4":"value4"})
+print("Update::",dic)
+print("Items::",dic.items())
+print("Keys::",dic.keys())
+print("Values",dic.values())
+print("Get key1::",dic.get("key1"));
+print("Pop key2::",dic.pop("key2"))
+print(dic)
